823da5e5407385e7e1656dbaee8132390a31cda5
wolderufael <wolderuphaelkassahun@gmail.com>
2024-08-22 10:47:08 +0300

    data quality check

--- scripts/analysis_script.py
+++ scripts/analysis_script.py
@@ -2,3 +2,7 @@ import pandas as pd
 
 def summary_statistics(df):
     return df.describe()
+def data_quality_check(df):
+    missing_values = df.isnull().sum()
+    negative_values = (df[['GHI', 'DNI', 'DHI']] < 0).sum()
+    return missing_values, negative_values
