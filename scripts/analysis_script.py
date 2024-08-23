6dbd9b25d3311a00ce51c8b716309f79bfc94fc8
wolderufael <wolderuphaelkassahun@gmail.com>
2024-08-23 12:52:32 +0300

    mean,median,std comparison part 2

--- scripts/analysis_script.py
+++ scripts/analysis_script.py
@@ -47,14 +47,14 @@ def summary_statistics(df):
     return df.describe()
 
 def mean_median_std_comparison(df1,df2,df3):
-    #mean comparison plot
-    mean_ghi_1 = df1['GHI'].mean()
+    #mean comparison plot of GHI
+    meann_DNI_1 = df1['GHI'].mean()
     mean_ghi_2 = df2['GHI'].mean()
     mean_ghi_3 = df3['GHI'].mean()
     
     data = {
     'Site': ['Benin', 'Sierraleone', 'Togo'],
-    'Mean GHI': [mean_ghi_1, mean_ghi_2, mean_ghi_3]
+    'Mean GHI': [meann_DNI_1, mean_ghi_2, mean_ghi_3]
     }
     mean_df = pd.DataFrame(data)
     plt.figure(figsize=(10, 6))
@@ -66,49 +66,166 @@ def mean_median_std_comparison(df1,df2,df3):
     plt.title('Comparison of Mean GHI Across Three Sites')
     plt.xlabel('Site')
     plt.ylabel('Mean GHI')
-    # plt.legend()
-    plt.grid(True)
     plt.show()
-    #median comparison plot
+    #median comparison plot of GHI
     median_ghi_1 = df1['GHI'].median()
     median_ghi_2 = df2['GHI'].median()
     median_ghi_3 = df3['GHI'].median()
     
     data = {
     'Site': ['Benin', 'Sierraleone', 'Togo'],
-    'Mean GHI': [median_ghi_1, median_ghi_2, median_ghi_3]
+    'Median GHI': [median_ghi_1, median_ghi_2, median_ghi_3]
     }
     median_df = pd.DataFrame(data)
     plt.figure(figsize=(10, 6))
-    plt.bar(median_df['Site'], median_df['Mean GHI'], color=['blue', 'green', 'orange'])
+    bars=plt.bar(median_df['Site'], median_df['Median GHI'], color=['blue', 'green', 'orange'])
+    
+    plt.bar_label(bars, fmt='%.2f')
 
     plt.title('Comparison of Median GHI Across Three Sites')
     plt.xlabel('Site')
-    plt.ylabel('Mean GHI')
-    plt.legend()
-    plt.grid(True)
+    plt.ylabel('Median GHI')
     plt.show()
-    
+    #standard deviation comparison plot of GHI
     std_ghi_1 = df1['GHI'].std()
     std_ghi_2 = df2['GHI'].std()
     std_ghi_3 = df3['GHI'].std()
     
     data = {
     'Site': ['Benin', 'Sierraleone', 'Togo'],
-    'Mean GHI': [std_ghi_1 , std_ghi_2 , std_ghi_3 ]
+    'Std GHI': [std_ghi_1 , std_ghi_2 , std_ghi_3 ]
     }
     std_df = pd.DataFrame(data)
     plt.figure(figsize=(10, 6))
-    plt.bar(std_df['Site'], std_df['Mean GHI'], color=['blue', 'green', 'orange'])
+    bars=plt.bar(std_df['Site'], std_df['Std GHI'], color=['blue', 'green', 'orange'])
+    
+    plt.bar_label(bars, fmt='%.2f')
 
     plt.title('Comparison of Standard Deviation of GHI Across Three Sites')
     plt.xlabel('Site')
-    plt.ylabel('Mean GHI')
-    plt.legend()
-    plt.grid(True)
+    plt.ylabel('Std GHI')
     plt.show()
     
+    ###########################################
+        #mean comparison plot of DNI
+    mean_dni_1 = df1['DNI'].mean()
+    mean_dni_2 = df2['DNI'].mean()
+    mean_dni_3 = df3['DNI'].mean()
     
+    data = {
+    'Site': ['Benin', 'Sierraleone', 'Togo'],
+    'Mean DNI': [mean_dni_1, mean_dni_2, mean_dni_3]
+    }
+    mean_df = pd.DataFrame(data)
+    plt.figure(figsize=(10, 6))
+    bars= plt.bar(mean_df['Site'], mean_df['Mean DNI'],color=['blue', 'green', 'orange'])
+    
+    plt.bar_label(bars, fmt='%.2f')
+
+    
+    plt.title('Comparison of Mean DNI Across Three Sites')
+    plt.xlabel('Site')
+    plt.ylabel('Mean DNI')
+    plt.show()
+    #median comparison plot of DNI
+    median_dni_1 = df1['DNI'].median()
+    median_dni_2 = df2['DNI'].median()
+    median_dni_3 = df3['DNI'].median()
+    
+    data = {
+    'Site': ['Benin', 'Sierraleone', 'Togo'],
+    'Median DNI': [median_dni_1, median_dni_2, median_dni_3]
+    }
+    median_df = pd.DataFrame(data)
+    plt.figure(figsize=(10, 6))
+    bars=plt.bar(median_df['Site'], median_df['Median DNI'], color=['blue', 'green', 'orange'])
+    
+    plt.bar_label(bars, fmt='%.2f')
+
+    plt.title('Comparison of Median DNI Across Three Sites')
+    plt.xlabel('Site')
+    plt.ylabel('Median DNI')
+    plt.show()
+    #standard deviation comparison plot of DNI
+    std_dni_1 = df1['DNI'].std()
+    std_dni_2 = df2['DNI'].std()
+    std_dni_3 = df3['DNI'].std()
+    
+    data = {
+    'Site': ['Benin', 'Sierraleone', 'Togo'],
+    'Std DNI': [std_dni_1 , std_dni_2 , std_dni_3 ]
+    }
+    std_df = pd.DataFrame(data)
+    plt.figure(figsize=(10, 6))
+    bars=plt.bar(std_df['Site'], std_df['Std DNI'], color=['blue', 'green', 'orange'])
+    
+    plt.bar_label(bars, fmt='%.2f')
+
+    plt.title('Comparison of Standard Deviation of DNI Across Three Sites')
+    plt.xlabel('Site')
+    plt.ylabel('Std DNI')
+    plt.show()
+    ###########################################
+    ###########################################
+    #mean comparison plot of DHI
+    mean_dhi_1 = df1['DHI'].mean()
+    mean_dhi_2 = df2['DHI'].mean()
+    mean_dhi_3 = df3['DHI'].mean()
+    
+    data = {
+    'Site': ['Benin', 'Sierraleone', 'Togo'],
+    'Mean DHI': [mean_dhi_1, mean_dhi_2, mean_dhi_3]
+    }
+    mean_df = pd.DataFrame(data)
+    plt.figure(figsize=(10, 6))
+    bars= plt.bar(mean_df['Site'], mean_df['Mean DHI'],color=['blue', 'green', 'orange'])
+    
+    plt.bar_label(bars, fmt='%.2f')
+
+    
+    plt.title('Comparison of Mean DHI Across Three Sites')
+    plt.xlabel('Site')
+    plt.ylabel('Mean DHI')
+    plt.show()
+    #median comparison plot of DHI
+    median_dhi_1 = df1['DHI'].median()
+    median_dhi_2 = df2['DHI'].median()
+    median_dhi_3 = df3['DHI'].median()
+    
+    data = {
+    'Site': ['Benin', 'Sierraleone', 'Togo'],
+    'Median DHI': [median_dhi_1, median_dhi_2, median_dhi_3]
+    }
+    median_df = pd.DataFrame(data)
+    plt.figure(figsize=(10, 6))
+    bars=plt.bar(median_df['Site'], median_df['Median DHI'], color=['blue', 'green', 'orange'])
+    
+    plt.bar_label(bars, fmt='%.2f')
+
+    plt.title('Comparison of Median DHI Across Three Sites')
+    plt.xlabel('Site')
+    plt.ylabel('Median DHI')
+    plt.show()
+    #standard deviation comparison plot of DHI
+    std_dhi_1 = df1['DHI'].std()
+    std_dhi_2 = df2['DHI'].std()
+    std_dhi_3 = df3['DHI'].std()
+    
+    data = {
+    'Site': ['Benin', 'Sierraleone', 'Togo'],
+    'Std DHI': [std_dhi_1 , std_dhi_2 , std_dhi_3 ]
+    }
+    std_df = pd.DataFrame(data)
+    plt.figure(figsize=(10, 6))
+    bars=plt.bar(std_df['Site'], std_df['Std DHI'], color=['blue', 'green', 'orange'])
+    
+    plt.bar_label(bars, fmt='%.2f')
+
+    plt.title('Comparison of Standard Deviation of DNI Across Three Sites')
+    plt.xlabel('Site')
+    plt.ylabel('Std DHI')
+    plt.show()
+    ########################
     
 def line_graph_over_a_day(df):
     df['Timestamp'] = pd.to_datetime(df['Timestamp'])
