3d31f425cb436640390d40c15ea1fe736f515812
wolderufael <wolderuphaelkassahun@gmail.com>
2024-08-23 12:14:10 +0300

    mean,median and standard deviation comparison of three sites

--- scripts/analysis_script.py
+++ scripts/analysis_script.py
@@ -5,13 +5,15 @@ import matplotlib.pyplot as plt
 import seaborn as sb
 
 
-def summary_statistics(df):
-    return df.describe()
-def data_quality_check(df):
+def check_missing_value(df):
     missing_values = df.isnull().sum()
-    negative_values = (df[['GHI', 'DNI', 'DHI']] < 0).sum()
+    # negative_values = (df[['GHI', 'DNI', 'DHI']] < 0).sum()
     return missing_values
 
+def replace_nagative_values(df):
+     df_copy = df.copy()
+     df_copy[['GHI', 'DNI', 'DHI']] = df_copy[['GHI', 'DNI', 'DHI']].clip(lower=0)
+     return df_copy
 
 def outliers_fun(df, cols):    
     outliers_arr = []
@@ -23,7 +25,7 @@ def outliers_fun(df, cols):
         # Calculate Z-scores for the entire column
         z_scores = stats.zscore(df_copy[col].dropna())
         outlier_mask = (abs(z_scores) > threshold)
-        print("----------------mask------------",outlier_mask[674:])
+        # print("----------------mask------------",outlier_mask[674:])
         
         # Collect outliers
         outliers = df_copy[col].dropna()[outlier_mask]
@@ -41,6 +43,73 @@ def outliers_fun(df, cols):
     
     return df_copy
 
+def summary_statistics(df):
+    return df.describe()
+
+def mean_median_std_comparison(df1,df2,df3):
+    #mean comparison plot
+    mean_ghi_1 = df1['GHI'].mean()
+    mean_ghi_2 = df2['GHI'].mean()
+    mean_ghi_3 = df3['GHI'].mean()
+    
+    data = {
+    'Site': ['Benin', 'Sierraleone', 'Togo'],
+    'Mean GHI': [mean_ghi_1, mean_ghi_2, mean_ghi_3]
+    }
+    mean_df = pd.DataFrame(data)
+    plt.figure(figsize=(10, 6))
+    bars= plt.bar(mean_df['Site'], mean_df['Mean GHI'],color=['blue', 'green', 'orange'])
+    
+    plt.bar_label(bars, fmt='%.2f')
+
+    
+    plt.title('Comparison of Mean GHI Across Three Sites')
+    plt.xlabel('Site')
+    plt.ylabel('Mean GHI')
+    # plt.legend()
+    plt.grid(True)
+    plt.show()
+    #median comparison plot
+    median_ghi_1 = df1['GHI'].median()
+    median_ghi_2 = df2['GHI'].median()
+    median_ghi_3 = df3['GHI'].median()
+    
+    data = {
+    'Site': ['Benin', 'Sierraleone', 'Togo'],
+    'Mean GHI': [median_ghi_1, median_ghi_2, median_ghi_3]
+    }
+    median_df = pd.DataFrame(data)
+    plt.figure(figsize=(10, 6))
+    plt.bar(median_df['Site'], median_df['Mean GHI'], color=['blue', 'green', 'orange'])
+
+    plt.title('Comparison of Median GHI Across Three Sites')
+    plt.xlabel('Site')
+    plt.ylabel('Mean GHI')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+    
+    std_ghi_1 = df1['GHI'].std()
+    std_ghi_2 = df2['GHI'].std()
+    std_ghi_3 = df3['GHI'].std()
+    
+    data = {
+    'Site': ['Benin', 'Sierraleone', 'Togo'],
+    'Mean GHI': [std_ghi_1 , std_ghi_2 , std_ghi_3 ]
+    }
+    std_df = pd.DataFrame(data)
+    plt.figure(figsize=(10, 6))
+    plt.bar(std_df['Site'], std_df['Mean GHI'], color=['blue', 'green', 'orange'])
+
+    plt.title('Comparison of Standard Deviation of GHI Across Three Sites')
+    plt.xlabel('Site')
+    plt.ylabel('Mean GHI')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+    
+    
+    
 def line_graph_over_a_day(df):
     df['Timestamp'] = pd.to_datetime(df['Timestamp'])
     filtered_df = df[df['Timestamp'].dt.strftime('%Y-%m-%d') == '2022-01-01']
@@ -60,6 +129,7 @@ def line_graph_over_a_day(df):
     plt.xticks(rotation=45)
     plt.tight_layout()
     plt.show()
+    
 def line_graph_over_a_month(df):
     df['Timestamp'] = pd.to_datetime(df['Timestamp'])
     filtered_df = df[df['Timestamp'].dt.strftime('%Y-%m') == '2022-01']
@@ -85,7 +155,6 @@ def correlation(df):
     plt.figure(figsize=(10, 6))
     sb.heatmap(df_correlation,annot=True)
     sb.pairplot(df_correlation)
-    print("1 2 3 4 5 6 7 8 9 10 11 yyyyyyhkhhhjhj---------------------")
-    # plt.show()
+    plt.show()
     
     return df_correlation
